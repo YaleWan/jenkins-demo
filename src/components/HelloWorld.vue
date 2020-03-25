bf5ec37f2edea75ba6d99bd3a06d9b462a6d8b69
wyh <yale_wan@163.com>
2020-03-25 21:35:39 +0800

    :fit 修改代码

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,16 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-03-25 16:40:20
+ * @LastEditTime: 2020-03-25 21:35:31
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /jenkins-demo/src/components/HelloWorld.vue
+ -->
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+      For a guide and recipes on how to configure / customize this project11111,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
