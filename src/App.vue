b3af23a9ab7b4c99be3dfccc321af9dfa7981ed6
wyh <yale_wan@163.com>
2020-03-27 22:45:53 +0800

    :fit 修改bug

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,4 @@
-<!--
- * @Author: your name
- * @Date: 2020-03-25 16:40:20
- * @LastEditTime: 2020-03-27 22:41:33
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /jenkins-demo/src/App.vue
- -->
+
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
