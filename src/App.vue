eee67cac6a2b834315fd7a65b30b9f1521bbc9ad
wyh <yale_wan@163.com>
2020-03-27 22:41:40 +0800

    :fit 修改bug

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-03-25 16:40:20
+ * @LastEditTime: 2020-03-27 22:41:33
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /jenkins-demo/src/App.vue
+ -->
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>hello world </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
